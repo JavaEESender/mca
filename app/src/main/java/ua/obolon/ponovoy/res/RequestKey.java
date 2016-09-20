73b2974e5f905eb8699fdf5470941ad43ee1edb7
Alexander <alex.it.admin@gmail.com>
2016-09-20 16:56:57 +0300

    all work

--- app/src/main/java/ua/obolon/ponovoy/res/RequestKey.java
+++ app/src/main/java/ua/obolon/ponovoy/res/RequestKey.java
@@ -4,5 +4,5 @@ package ua.obolon.ponovoy.res;
  * Created by Alexander on 18.09.2016.
  */
 public enum RequestKey {
-    NEW_LOGIN, CALL_ANDROID, GET_ORDERS, MISSED_CALLS
+    NEW_LOGIN, CALL_ANDROID, GET_ORDERS, MISSED_CALLS, SET_NEW_CALL, SET_LIST_CALLS
 }
